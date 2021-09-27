d19fe69e174a7c307ba897991c03df640431f68f
tkdwns414 <sangjune2000@naver.com>
2021-09-27 20:33:41 +0900

    BOJ 9/27 fix

--- 단계별/24. DFS와 BFS/1697.py
+++ 단계별/24. DFS와 BFS/1697.py
@@ -3,7 +3,7 @@ from collections import deque
 
 n, k = map(int, sys.stdin.readline().split())
 
-dis = [-1 for _ in range(k + 1)]
+dis = [-1 for _ in range(10 ** 5 + 1)]
 dis[n] = 0
 
 queue = deque()
@@ -11,9 +11,10 @@ queue.append(n)
 
 while queue:
     s = queue.popleft()
+    if s == k:
+        print(dis[s])
+        break
     for next_s in [s - 1, s + 1, 2 * s]:
-        if 0 <= next_s <= k and dis[next_s] == -1:
+        if 0 <= next_s <= 10 ** 5 and dis[next_s] == -1:
             dis[next_s] = dis[s] + 1
             queue.append(next_s)
-
-print(dis[k])

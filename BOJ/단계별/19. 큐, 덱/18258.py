8ce4a17ca23caaa814cc3f149fdc9fbffcd3cf50
tkdwns414 <sangjune2000@naver.com>
2021-10-10 19:37:30 +0900

    BOJ 19단계 18258: 큐 2(S4)

---
+++ BOJ/단계별/19. 큐, 덱/18258.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline())
+queue = deque()
+
+for _ in range(n):
+    ins = list(sys.stdin.readline().split())
+    if ins[0] == "push":
+        queue.append(ins[1])
+    elif ins[0] == "pop":
+        if queue:
+            print(queue.popleft())
+        else:
+            print(-1)
+    elif ins[0] == "size":
+        print(len(queue))
+    elif ins[0] == "empty":
+        if queue:
+            print(0)
+        else:
+            print(1)
+    elif ins[0] == "front":
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+    elif ins[0] == "back":
+        if queue:
+            print(queue[len(queue) - 1])
+        else:
+            print(-1)

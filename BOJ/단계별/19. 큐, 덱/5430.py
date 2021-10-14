e36bd539a3128e6a0ed84ad530cbead772fe5a01
tkdwns414 <sangjune2000@naver.com>
2021-10-14 18:36:54 +0900

    BOJ 19단계 5430: AC(G5)

---
+++ BOJ/단계별/19. 큐, 덱/5430.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+
+t = int(sys.stdin.readline())
+
+for _ in range(t):
+    p = sys.stdin.readline().strip()
+    n = int(sys.stdin.readline())
+    my_deque = deque(eval(sys.stdin.readline().strip()))
+    reverse_state = False
+    error = False
+
+    for func in p:
+        if func == "R":
+            if reverse_state:
+                reverse_state = False
+            else:
+                reverse_state = True
+        else:
+            if my_deque:
+                if reverse_state:
+                    my_deque.pop()
+                else:
+                    my_deque.popleft()
+            else:
+                error = True
+                break
+
+    if error:
+        print("error")
+    else:
+        if reverse_state:
+            print("[" + ",".join(map(str, reversed(my_deque))) + "]")
+        else:
+            print("[" + ",".join(map(str, my_deque)) + "]")

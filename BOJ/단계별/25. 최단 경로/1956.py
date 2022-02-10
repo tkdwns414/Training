c51acb6a0345032899b8c13e501892e5f20587e7
tkdwns414 <sangjune2000@naver.com>
2022-02-10 22:01:27 +0900

    BOJ 25단계 1956: 운동(G4)

---
+++ BOJ/단계별/25. 최단 경로/1956.py
@@ -0,0 +1,22 @@
+import sys
+
+V, E = map(int, sys.stdin.readline().split())
+roads = []
+dist = [[int(1e9) for i in range(V)] for j in range(V)]
+
+for _ in range(E):
+    a, b, c = map(int, sys.stdin.readline().split())
+    dist[a - 1][b - 1] = c
+
+for i in range(V):
+    for j in range(V):
+        for k in range(V):
+            dist[j][k] = min(dist[j][k], dist[j][i] + dist[i][k])
+
+result = int(1e9)
+for i in range(V):
+    result = min(result, dist[i][i])  # 본인에서 본인으로 돌아오는 최소값으로 갱신
+if result == int(1e9):
+    print(-1)
+else:
+    print(result)
